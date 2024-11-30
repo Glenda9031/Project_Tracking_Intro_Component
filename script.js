a6e3c585885af4c86304144d1257c51ea8086222
Glenda Joyce Reed <100007624+Glenda9031@users.noreply.github.com>
2024-11-30 15:42:59 -0600

    Update index.html, style.css and script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+const menuIcon = document.querySelector('.menu-icon');
+menuIcon.addEventListener('click', () => {
+    menuIcon.classList.toggle('show');
+    menuIcon.nextElementSibling.classList.toggle('show');
+});
+
